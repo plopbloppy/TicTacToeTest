8b5254a2ea1d8971455cb4672ca1cd701dfc424c
Kaili Tang <170157589+plopbloppy@users.noreply.github.com>
2025-03-27 18:22:57 -0400

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -7,58 +7,69 @@ public class TicTacToe {
         return turn;
     }
 
-    public static void board(String[][] board, int i, int userInput) {
+    public static void board(String[][] board, int i) {
         for (int row = 0; row < board.length; row++) {
             for (int col = 0; col < board[row].length; col++) {
-                if (board[row][col].contains(Integer.toString(userInput))) {
-                    board[row][col] = turn(i - 1);
-                }
                 System.out.print(board[row][col]);
             }
             System.out.println();
         }
     }
 
-    // public static boolean hasSpace(String[][] board, int userInput) {
-    //     boolean hasSpace = true;
+    public static void placePiece(String[][] board, int i, String userInput) {
+        String letter = userInput.substring(0, 1);
+        int number = Integer.parseInt(userInput.substring(1));
+        int col;
+        int row;
 
-    //     for (int row = 0; row < board.length; row++) {
-    //         for (int col = 0; col < board[row].length; col++) {
-    //             hasSpace = !board[row][col].contains("o") || !board[row][col].contains("x") ? false : true; 
-    //         }
-    //     } 
-    //     return hasSpace;
-    // }
-    
-    // public static String checkForWins() {
-        
-    // }
+        switch (letter) {
+            case "A": 
+                col = 1;
+                break;
+            case "B":
+                col = 2;
+                break;
+            case "C": 
+                col = 3;
+                break;
+        }
+        switch (number) {
+            case 1: 
+                row = 2;
+                break;
+            case 2:
+                row = 3;
+                break;
+            case 3: 
+                row = 4;
+                break;
+        } 
+        board[col][row] = turn(i);
+    }
 
     public static void main(String[] args) throws Exception {
         Scanner keyboard = new Scanner(System.in);
         final int MAX_TOLERANCE = 9;
-        int userInput = 0;
 
-        String[][] board = { { "  ", " A  ", " B  ", " C  "},
-                { "1 ", "___|", "___|", "___" },
-                { "2 ", "___|", "___|", "___" },
-                { "3 ", "___|", "___|", "___" } };
-            
+        String[][] board = { { "   ", " A ", "B ", "C" },
+                { "   ", " _ ", "_ ", "_" },
+                { "1  ", "|_", "|_", "|_|" },
+                { "2  ", "|_", "|_", "|_|" },
+                { "3  ", "|_", "|_", "|_|" } };
+
         System.out.println("Welcome to Tic-Tac-Toe!");
         System.out.println(
                 "In this game, your objective is to have three consequence pieces diagonally or in a row/column.");
         System.out.println("'o' plays first, and 'x' plays second.");
-        System.out.println("To place a piece, enter the coordinates in the form of 'A1'.");
+        System.out.println("To place your piece, enter the column and the row, in the form of 'A1'.");
 
-        for (int i = 0; i < MAX_TOLERANCE; i++) { 
-            try {
-                board(board, i, userInput);
-                System.out.println("Turn: " + turn(i));
-                System.out.println("Please enter a coordinate to play:");
-                userInput = keyboard.nextInt();
-            } catch (InputMismatchException exception) {
-                System.out.println("Not a number from 1 to 9!");
-            }
+        for (int i = 0; i < MAX_TOLERANCE; i++) {
+            board(board, i);
+            System.out.println("Turn: " + turn(i));
+
+            System.out.println("Please enter your coordinates:");
+            String input = keyboard.next();
+            placePiece(board, i, input);
         }
     }
 }
